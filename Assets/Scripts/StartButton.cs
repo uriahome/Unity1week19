76b86e3468749b9efa3b7b38f867afc1fefbeb3c
uria <uria1106@gmail.com>
2021-04-26 16:53:47 +0900

    スタートボタンを配置

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOScale(1.3f,1.0f).SetEase(Ease.OutElastic).SetLoops(-1,LoopType.Restart);//拡大縮小を繰り返す
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
