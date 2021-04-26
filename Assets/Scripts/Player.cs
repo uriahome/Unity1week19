de89e8c2a86f5d733cc9f0fcf115601cee85be8d
uria <uria1106@gmail.com>
2021-04-26 16:35:11 +0900

    test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("nyaa");
     }
 
     // Update is called once per frame
