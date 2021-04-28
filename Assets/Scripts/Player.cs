82a3be4c4c66abaaf51ed303d70a30f79f3cf588
uria <uria1106@gmail.com>
2021-04-28 22:52:01 +0900

    とりあえず簡易的にキャラを左右に動かす

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    /*[SerializeField] Rigidbody2D rigid2d;
+    [SerializeField] float walkForce = 30.0f;
+    [SerializeField] float maxWalkSpeed = 2.0f;
+    //[SerializeField] int key;
+
+    [SerializeField] float speed;
+    */
+
     void Start()
     {
+        //this.rigid2d = GetComponent<Rigidbody2D>();
         Debug.Log("nyaa");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*key = 0;
+
+        if(Input.GetKey(KeyCode.RightArrow)){
+            key = 1;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            key = -1;
+        }
+        */
+
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(1,0,0);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(-1,0,0);
+        }
+
+        /*speed = Mathf.Abs(this.rigid2d.velocity.x);
+        if(speed < this.maxWalkSpeed){
+            this.rigid2d.AddForce(transform.right*key*this.walkForce);
+        }*/
     }
 }
