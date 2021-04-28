59bf6b35c22da35fa3134459bfd0b27384ae57e0
uria <uria1106@gmail.com>
2021-04-28 23:03:15 +0900

    弾の移動の実装

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;//速度
+    public Vector3 MoveVelocity;//進むべき方向
+    void Start()
+    {
+        this.gameObject.transform.rotation = Quaternion.Euler(0.0f,0.0f,0.0f);
+        MoveVelocity = gameObject.transform.rotation * new Vector3(0.0f,speed,0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //transform.Translate(0,0.05f,0);
+    }
+    void Move(){
+        gameObject.transform.position += MoveVelocity*Time.deltaTime;
+    }
+    void OnBecameInvisible(){//画面外に出たら消える
+        Destroy(this.gameObject);
+    }
+}
