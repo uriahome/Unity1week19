9de2c8bf0184a1cee238f5895873acf37069cda9
uria <uria1106@gmail.com>
2021-05-13 18:53:37 +0900

    clockとかの移動用にItemスクリプトを作成した

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] Vector2 direction;//移動する方向
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+        Move();//移動する力を一度だけ与える
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {//真下に落ちていく
+        direction = new Vector2(0,-1.0f);
+        rb2d.velocity = direction * speed;
+    }
+}
